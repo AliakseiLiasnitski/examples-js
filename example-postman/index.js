d1e8dc4ecd5c03793a3e55b9fb06076743fbbb7e
AliakseiLiasnitski <alex.lesnicky@gmail.com>
2024-01-17 13:13:45 +0300

    feat: use rp postman collection for testing

--- example-postman/index.js
+++ example-postman/index.js
@@ -3,8 +3,42 @@ const newman = require('newman');
 const SKIP_RP_FLAG = '--skip-rp';
 const hasSkipRPFlag = process.argv.indexOf(SKIP_RP_FLAG) !== -1;
 
+const environment = {
+  values: [
+    {
+      key: 'username',
+      type: 'string',
+      value: process.env.TEST_USER_NAME,
+    },
+    {
+      key: 'admin.password',
+      type: 'string',
+      value: process.env.TEST_USER_PASSWORD,
+    },
+    {
+      key: 'url',
+      type: 'string',
+      value: process.env.TEST_URL,
+    },
+    {
+      key: 'host',
+      type: 'string',
+      value: process.env.TEST_HOST,
+    },
+    {
+      key: 'port',
+      type: 'string',
+      value: process.env.TEST_PORT,
+    },
+  ],
+};
+
 hasSkipRPFlag
-  ? newman.run({ collection: './collections/Example.postman_collection.json', reporters: 'cli' })
+  ? newman.run({
+      collection: './collections/Example.postman_collection.json',
+      reporters: 'cli',
+      environment,
+    })
   : newman.run(
       {
         collection: './collections/Example.postman_collection.json',
@@ -16,6 +50,7 @@ hasSkipRPFlag
             launch: process.env.RP_LAUNCH_NAME || 'LAUNCH_NAME',
             project: process.env.RP_PROJECT_NAME || 'PROJECT_NAME',
             description: 'PROJECT_DESCRIPTION',
+            environment,
             attributes: [
               {
                 key: 'attributeKey',
