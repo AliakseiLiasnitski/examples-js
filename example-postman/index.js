c95f956f2fc3cea900a1072606dc7da4903c1c07
AliakseiLiasnitski <alex.lesnicky@gmail.com>
2024-01-17 10:59:13 +0300

    feat: add a pipeline to run tests without rp

--- example-postman/index.js
+++ example-postman/index.js
@@ -1,32 +1,37 @@
 const newman = require('newman');
 
-newman.run(
-  {
-    collection: './collections/Example.postman_collection.json',
-    reporters: '@reportportal/agent-js-postman',
-    reporter: {
-      '@reportportal/agent-js-postman': {
-        endpoint: 'http://your-instance.com:8080/api/v1',
-        token: '00000000-0000-0000-0000-000000000000',
-        launch: 'LAUNCH_NAME',
-        project: 'PROJECT_NAME',
-        description: 'PROJECT_DESCRIPTION',
-        attributes: [
-          {
-            key: 'attributeKey',
-            value: 'attrbuteValue',
+const SKIP_RP_FLAG = '--skip-rp';
+const hasSkipRPFlag = process.argv.indexOf(SKIP_RP_FLAG) !== -1;
+
+hasSkipRPFlag
+  ? newman.run({ collection: './collections/Example.postman_collection.json' })
+  : newman.run(
+      {
+        collection: './collections/Example.postman_collection.json',
+        reporters: '@reportportal/agent-js-postman',
+        reporter: {
+          '@reportportal/agent-js-postman': {
+            endpoint: process.env.RP_ENDPOINT || 'http://your-instance.com:8080/api/v1',
+            apiKey: process.env.RP_API_KEY || '00000000-0000-0000-0000-000000000000',
+            launch: process.env.RP_LAUNCH_NAME || 'LAUNCH_NAME',
+            project: process.env.RP_PROJECT_NAME || 'PROJECT_NAME',
+            description: 'PROJECT_DESCRIPTION',
+            attributes: [
+              {
+                key: 'attributeKey',
+                value: 'attrbuteValue',
+              },
+              {
+                value: 'secondAttrbuteValue',
+              },
+            ],
+            debug: true,
           },
-          {
-            value: 'secondAttrbuteValue',
-          },
-        ],
-        debug: true,
+        },
       },
-    },
-  },
-  function (err) {
-    if (err) {
-      throw err;
-    }
-  },
-);
+      function (err) {
+        if (err) {
+          throw err;
+        }
+      },
+    );
